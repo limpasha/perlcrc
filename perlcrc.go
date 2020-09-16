8a8aacfb6eeeee86814fa249b875fd2fc6fdc4b0
Pavel Limorenko <p.limorenko@corp.mail.ru>
2020-09-16 12:59:05 +0300

    remove main() func from module

--- perlcrc.go
+++ perlcrc.go
@@ -4,8 +4,6 @@
 // https://st.aticpan.org/source/LEEJO/String-CRC32-1.8/CRC32.xs
 package perlcrc
 
-import "fmt"
-
 var crcTable = []uint32{
 	0x0, 0x77073096, 0xee0e612c, 0x990951ba, 0x76dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,
 	0xedb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988, 0x9b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91,
@@ -51,15 +49,4 @@ func CRC32String(chars string) uint32 {
 	}
 
 	return crc ^ 0xFFFFFFFF
-}
-
-func main() {
-	for _, str := range []string{
-		"12ioj@!#",
-		"daskdjos",
-		"mail@mail.ru",
-		"das sad912 &*@!0",
-	} {
-		fmt.Println(CRC32String(str))
-	}
 }
